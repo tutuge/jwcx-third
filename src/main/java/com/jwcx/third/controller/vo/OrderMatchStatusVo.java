b616d658624a9d4920862bb7c2cc05484587794e
王斌 <850083043@qq.com>
2023-07-13 19:50:56 +0800

    字段类型修改

--- src/main/java/com/jwcx/third/controller/vo/OrderMatchStatusVo.java
+++ src/main/java/com/jwcx/third/controller/vo/OrderMatchStatusVo.java
@@ -4,14 +4,16 @@ package com.jwcx.third.controller.vo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class OrderMatchStatusVo {
 
     @Schema(description = "订单匹配状态，0：未匹配，1：已匹配")
-    private String status;
+    private Integer status;
 
     @Schema(description = "订单匹配时间")
-    private String matchTime;
+    private Date matchTime;
 
     @Schema(description = "司机信息")
     private DriverInfoVo driverInfoVo;

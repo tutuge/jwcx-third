1a37717fcb1bb191ca42b5360934ec56303a81e4
王斌 <850083043@qq.com>
2023-07-13 23:47:37 +0800

    测试地址与时间字段转义修改

--- src/main/java/com/jwcx/third/controller/vo/OrderMatchStatusVo.java
+++ src/main/java/com/jwcx/third/controller/vo/OrderMatchStatusVo.java
@@ -1,6 +1,7 @@
 package com.jwcx.third.controller.vo;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -12,6 +13,7 @@ public class OrderMatchStatusVo {
     @Schema(description = "订单匹配状态，0：未匹配，1：已匹配")
     private Integer status;
 
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     @Schema(description = "订单匹配时间")
     private Date matchTime;
 

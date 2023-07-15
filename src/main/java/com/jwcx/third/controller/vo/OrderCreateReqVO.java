6d90f2d4ae88ea1a732676b4ff2d3b2b5fa9adf9
王斌 <850083043@qq.com>
2023-07-15 23:14:39 +0800

    测试地址与时间字段转义修改

--- src/main/java/com/jwcx/third/controller/vo/OrderCreateReqVO.java
+++ src/main/java/com/jwcx/third/controller/vo/OrderCreateReqVO.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+
 @Schema(description ="管理后台 - 订单创建 Request VO")
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -14,5 +16,11 @@ public class OrderCreateReqVO extends OrderBaseVO {
      * 是否是第三方订单
      */
     private Boolean third = false;
+
+    /**
+     * 回调地址
+     */
+    @NotBlank(message = "回调地址不得为空")
+    private String callback;
 
 }
